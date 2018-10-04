f68f76792e2a288ce98323ca197089dc93b99000
Ann Szalkowski <MariaSzalkowski@Anns-MacBook-Pro.local>
2018-10-04 16:48:06 -0500

    first commit

---
+++ scripts/R_script1.R
@@ -0,0 +1,68 @@
+# My first R script
+# created: 10/1/18
+#Updated: 10/1/18
+# Maria Szalkowski
+
+#R Operations
+  #arithmatic
+2+2
+2 * 2
+2/2
+#power
+2^2
+
+#order of operations works in the same way
+
+2 + 2 *3
+
+# Functions
+
+sqrt(225)
+
+#combining function
+#use parenthasis
+
+sqrt(abs(-8))
+
+#creating vector
+
+my_vector = c(1,2,3)
+
+#multiple arguments to a function
+
+round(3.124, 2)
+#second number is number of decimals to round to
+
+#search functions, R studio helps
+#ask for help:
+?abs
+
+#tab completions: type tab before finished typing variable name and the name with pop up
+
+#up and down arrows in console can pull up past input
+
+--------
+  
+#Topic 2: Variables
+  
+  #numberic data
+  #asign variables
+  item1 = 20
+  #or
+    item <- 20
+   item2 = 10
+ 
+   #math operations with variables
+    item3 = item1 + item2
+    
+  #character data
+    #same as string
+    string = "a word"
+    
+    
+
+
+  
+
+
+
